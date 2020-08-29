9b029a14c2d270e5d22333cb11f4f744336d6a22
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-08-28 19:37:40 -0700

    adding apiroute file

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/apiRoutes')(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log(`App listening on PORT:${PORT}`);
