3dcd4b41b898be5872cc26a5fa731e4dc741531c
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-08-28 19:32:21 -0700

    adding server file

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,16 @@
+// creating the server here
+const express = require('express');
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+
+app.listen(PORT, function () {
+    console.log(`App listening on PORT:${PORT}`);
+  });
+  
